b6d8687b4e6cde6b2152ccadb411ff6ab4c2e1f6
samer-- <s.abdallah@ucl.ac.uk>
2016-12-22 16:48:33 +0000

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(prism).
-version('0.1.1').
+version('0.1.2').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Run PRISM as a child process').
 download('https://github.com/samer--/prism.git').
